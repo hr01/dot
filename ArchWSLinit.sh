e7ae166ae6cf0c66d9b00ed87ffb8ea28472edb8
HR01 <omanko-kowareru@outlook.com>
2019-12-09 07:32:06 +0000

    update

--- ArchWSLinit.sh
+++ ArchWSLinit.sh
@@ -1,2 +1,25 @@
+echo pacman-key initting (1/)
 pacman-key --init;pacman-key --populate
+echo pacman package updating... (2/)
 pacman -Syyuu $INSTPACKAGE
+echo yay installing... (3/)
+wget https://github.com/koumaza/pacman-my-init/blob/master/yay-9.4.2-1-x86_64.pkg.tar.xz;pacman -U yay-9.4.2-1-x86_64.pkg.tar.xz
+yay -Syyuu
+echo fish initting... (4/)
+read -p "$(echo -e "\e[;1m""Fisher_config Anyway?"" (Y/N): ")" yn; case "$yn" in [yY]*) echo -e "\e[33m";; *) echo -e "\e[1m""exit" && exit;; esac
+echo -e "\e[m"
+fish -c "curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher"
+fish -c "fisher add oh-my-fish/theme-bobthefish"
+fish -c "fisher add fisherman/docker-completion"
+fish -c "fisher add fisherman/fzf"
+fish -c "pacman -Sy fzf"
+fish -c "fisher add fisherman/spin"
+fish -c "fisher add fisherman/z"
+fish -c "fisher add edc/bass"
+fish -c "fisher add 0rax/fish-bd"
+fish -c "fisher add oh-my-fish/plugin-balias"
+echo Other Settings (5/5)
+echo -e "\e[;1m""Set Your Use Editor"
+read youruseeditor
+export EDITOR=$youruseeditor
+wget "https://gist.github.com/koumaza/51452707aad4244e6d0ee951756bebab/raw/6b0a4f6fc96083d71f43a6a1a8c27b8ceaab7e38/useradd.sh";chmod a+x useradd.sh;bash useradd.sh
--- ENVARCH.sh
+++ ENVARCH.sh
@@ -1,2 +1,2 @@
-set -gx INSTPACKAGE ""
-wget ;chmod a+x ArchWSLinit.sh;bash ArchWSLinit.sh
+set -gx INSTPACKAGE "fish vim neovim git less progress neofetch"
+wget https://raw.githubusercontent.com/hr01/dot/master/ArchWSLinit.sh;chmod a+x ArchWSLinit.sh;bash ArchWSLinit.sh
